a85826411939e0d1503ffb2a2d32291b8ef8ec93
Emmanuel Ametepee <49801256+EmmanuelAmet@users.noreply.github.com>
2021-07-08 16:02:19 -0700

    first commit all project dependencies, api, library integrated successful.

---
+++ app/src/main/java/com/emmanuelamet/customer_record_keeper/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.emmanuelamet.customer_record_keeper
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
